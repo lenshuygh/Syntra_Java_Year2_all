4ea7cc0a7d74560729d028b1d941e18a67ba9140
Lens Huygh <lens.huygh@intouch.be>
2019-11-02 14:41:14 +0100

    finishing the bowling TDD app: changed method signature to only take in a number of pins down, the throw is calculated, refactored adding turns to include tests on spare and strike

--- code/bowling/src/main/java/be/lens/syntra/junit/extra/BowlingGame.java
+++ code/bowling/src/main/java/be/lens/syntra/junit/extra/BowlingGame.java
@@ -7,26 +7,38 @@ public class BowlingGame {
     public boolean spare;
     public int score = 0;
     public boolean gameCanContinue = true;
+    public int ballNumber = 0;
 
-    public void calculateThrow(int ballthrow, int pinsDown) {
-        if(ballthrow == 1){
-            turn++;
-        }
+    public void calculateThrow(int pinsDown) {
+
         totalPinsDown += pinsDown;
-
-        if ((ballthrow == 0) && (totalPinsDown == 10)) {
+        spare= false;
+        strike = false;
+        if ((ballNumber == 0) && (totalPinsDown == 10)) {
             strike = true;
             spare = false;
         }
-        if ((ballthrow == 1) && (totalPinsDown == 10)) {
+        if ((ballNumber == 1) && (totalPinsDown == 10)) {
             strike = false;
             spare = true;
         }
-            score += pinsDown;
-        if(turn < 10){
+        score += pinsDown;
+        if (turn < 10) {
             gameCanContinue = true;
-        }else{
+        } else {
             gameCanContinue = false;
+        }
+        if (ballNumber == 1) {
+            if(!isSpare()){
+                turn++;
+                ballNumber = 0;
+            }
+        } else {
+            if(isStrike()) {
+                ballNumber = 0;
+            }else{
+                ballNumber = 1;
+            }
         }
     }
 
@@ -48,6 +60,10 @@ public class BowlingGame {
 
     public int getTurn() {
         return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
     }
 
     public boolean isGameCanContinue() {
--- code/bowling/src/test/java/be/lens/syntra/junit/extra/BowlingTest.java
+++ code/bowling/src/test/java/be/lens/syntra/junit/extra/BowlingTest.java
@@ -16,8 +16,8 @@ public class BowlingTest {
     @Test
     public void canPlay5Turns() {
         for (int i = 0; i < 5; i++) {
-            bowlingGame.calculateThrow(0, 0);
-            bowlingGame.calculateThrow(1, 0);
+            bowlingGame.calculateThrow( 0);
+            bowlingGame.calculateThrow( 0);
         }
         assertEquals(bowlingGame.isGameCanContinue(),true);
     }
@@ -25,51 +25,81 @@ public class BowlingTest {
     @Test
     public void stopsAfter10Turns(){
         for (int i = 0; i < 11; i++) {
-            bowlingGame.calculateThrow(0, 0);
-            bowlingGame.calculateThrow(1, 0);
+            bowlingGame.calculateThrow( 0);
+            bowlingGame.calculateThrow( 0);
         }
         assertEquals(bowlingGame.isGameCanContinue(),false);
     }
 
     @Test
     public final void ThrowDown10PinsInTurnOne() {
-        bowlingGame.calculateThrow(0, 10);
+        bowlingGame.calculateThrow( 10);
         assertEquals(bowlingGame.isStrike(), true);
         assertEquals(bowlingGame.isSpare(), false);
     }
 
     @Test
     public final void ThrowDown10PinsInTwoThrows() {
-        bowlingGame.calculateThrow(0, 5);
-        bowlingGame.calculateThrow(1, 5);
+        bowlingGame.calculateThrow( 5);
+        bowlingGame.calculateThrow( 5);
         assertEquals(bowlingGame.isSpare(), true);
         assertEquals(bowlingGame.isStrike(), false);
     }
 
     @Test
     public final void ThrowDownOnePinsIsOnePoint() {
-        bowlingGame.calculateThrow(0, 1);
+        bowlingGame.calculateThrow( 1);
         assertEquals(bowlingGame.getScore(), 1);
     }
 
     @Test
     public final void ThrowDownTwoPinsIsTwoPoints() {
-        bowlingGame.calculateThrow(0, 1);
-        bowlingGame.calculateThrow(1, 1);
+        bowlingGame.calculateThrow( 1);
+        bowlingGame.calculateThrow( 1);
         assertEquals(bowlingGame.getScore(), 2);
     }
 
     @Test
     public final void ThrowDownTwoPinsIsTwoPointsInOneThrow() {
-        bowlingGame.calculateThrow(0, 2);
+        bowlingGame.calculateThrow(2);
         assertEquals(bowlingGame.getScore(), 2);
     }
 
     @Test
     public final void aStrikeResultsIn10Points(){
-        bowlingGame.calculateThrow(0,10);
+        bowlingGame.calculateThrow(10);
         assertEquals(bowlingGame.getScore(),10);
     }
 
+    @Test
+    public final void afterAStrikePlayerGets2ThrowsAfterStrikeWith10Points(){
+        bowlingGame.calculateThrow(10);
+        assertEquals(bowlingGame.getScore(),10);
+        assertEquals(bowlingGame.getTurn(),0);
+        bowlingGame.calculateThrow(5);
+        assertEquals(bowlingGame.getScore(),15);
+        assertEquals(bowlingGame.getTurn(),0);
+        bowlingGame.calculateThrow(5);
+        assertEquals(bowlingGame.getScore(),20);
+        assertEquals(bowlingGame.getTurn(),1);
+    }
 
+    @Test
+    public final void afterTurn10ASpareGetsAnExtraTurn(){
+        bowlingGame.setTurn(10);
+        bowlingGame.calculateThrow(5);
+        bowlingGame.calculateThrow(5);
+        assertEquals(bowlingGame.isGameCanContinue(),false);
+    }
+
+    @Test
+    public final void afterTurn10AStrikeGets2ExtraThrows(){
+        bowlingGame.setTurn(10);
+        bowlingGame.calculateThrow(10);
+        assertEquals(bowlingGame.isGameCanContinue(),true);
+        bowlingGame.calculateThrow(5);
+        assertEquals(bowlingGame.isGameCanContinue(),true);
+        bowlingGame.calculateThrow(5);
+        assertEquals(bowlingGame.isGameCanContinue(),false);
+    }
 }
